e4cc1b2982d39ff72e3cc065c00873e2be2b43ec
canfelli25 <fellitacandini@gmail.com>
2022-06-11 21:23:29 +0700

    plan get and create

--- backend/tourguide/tourguideapi/serializers.py
+++ backend/tourguide/tourguideapi/serializers.py
@@ -1,5 +1,5 @@
 from .models import (
-    City, PointOfInterest
+    City, PointOfInterest, Plan, PlanItem
 )
 from rest_framework import serializers
 
@@ -13,4 +13,20 @@ class POISerializer(serializers.ModelSerializer):
         model = PointOfInterest
         fields = ['id', 'title', 'photo', 'category', 'description', 
             'address', 'price', 'open_time', 'avg_time_spent']
-   
+
+class PlanItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlanItem
+        fields = ['name', 'price', 'type', 'seq', 'ticket_url']
+
+class PlanSerializer(serializers.ModelSerializer):
+    plan_items = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Plan
+        fields = ['name', 'plan_items']
+
+    def get_plan_items(self, obj):
+        plan_items = PlanItem.objects.filter(plan=obj)
+        return PlanItemSerializer(plan_items, many=True).data
+
--- backend/tourguide/tourguideapi/views.py
+++ backend/tourguide/tourguideapi/views.py
@@ -1,11 +1,17 @@
+import logging
+
+from datetime import datetime
+
 from .models import (
-    City, PointOfInterest
+    City, PointOfInterest, Plan, PlanItem
 )
 from rest_framework import generics, viewsets
 from rest_framework.response import Response
 from .serializers import (
-    CitySerializer, POISerializer
+    CitySerializer, POISerializer, PlanSerializer
 )
+
+logger = logging.getLogger(__name__)
 
 class CityViewSet(viewsets.ViewSet):
     """
@@ -25,7 +31,6 @@ class POIList(generics.ListAPIView):
         by filtering against the three query parameter in the URL.
         """
         city_id = self.request.query_params.get('city_id')
-        print('city id', city_id)
         if not city_id is None:
             queryset = City.objects.get(id=int(city_id)).pois.all()
         else:
@@ -40,4 +45,48 @@ class POIList(generics.ListAPIView):
             queryset = queryset.filter(title__icontains=keyword)
 
         return queryset
+
+class OnGoingPlanViewset(viewsets.ViewSet):
+    def list(self, request):
+        year, month, day = self.__today_date()
+        try:
+            plan = Plan.objects.filter(created_at__year=year,
+                created_at__month=month, created_at__day=day)
+            serializer = PlanSerializer(
+                plan, 
+                many=True)
+        except Plan.DoesNotExist:
+            logger.warning("On going plan does not exist!")
+            serializer = PlanSerializer(None, many=True)
         
+        return Response(serializer.data)
+
+    def create(self, request):
+        data = request.data
+        items_data = data.pop('plan_items')
+        try:
+            plan = Plan.objects.create(**data)
+            for item_data in items_data:
+                PlanItem.objects.create(plan=plan, **item_data)
+        except Exception as e:
+            logger.error(e)
+            logger.error('Plan insertion failed.')
+
+            return Response({
+                'code': 401,
+                'isSuccess': False,
+                'error': {
+                    'message': 'Plan Creation Failed'
+                }
+            })
+
+        return Response({
+            'code': 200,
+            'isSuccess': True,
+            'error': None
+        })
+
+    def __today_date(self):
+        today = datetime.today()
+
+        return today.year, today.month, today.day
